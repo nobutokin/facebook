38225a9d9169f7319e1b374c327c1c0f677f94b6
Nobuto <nobuto0502@gmail.com>
2017-05-01 12:16:02 +0000

    コメント機能不具 合対応

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
+    getMessages
   end
 
   def index
@@ -29,11 +30,33 @@ class MessagesController < ApplicationController
     @message = @conversation.messages.build(message_params)
     if @message.save
       redirect_to conversation_messages_path(@conversation)
+    else
+      render 'index'
     end
   end
 
   private
     def message_params
       params.require(:message).permit(:body, :user_id)
+    end
+
+    def getMessages
+      @messages = @conversation.messages
+
+      if @messages.length > 10
+        @over_ten = true
+        @messages = @messages[-10..-1]
+      end
+
+      if params[:m]
+        @over_ten = false
+        @messages = @conversation.messages
+      end
+
+      if @messages.last
+        if @messages.last.user_id != current_user.id
+         @messages.last.read = true
+        end
+      end
     end
 end
